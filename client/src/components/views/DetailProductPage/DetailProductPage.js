58c177f4d1d551e25db0c67fc881784be47ccc54
dongukuklee <52775482+dongukuklee@users.noreply.github.com>
2021-01-18 23:03:07 +0900

    ProductInfo 추가

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -35,7 +35,7 @@ const DetailProductPage = (props) => {
         </Col>
 
         <Col lg={12} sm={24}>
-          <ProductInfo />
+          <ProductInfo detail={product} />
         </Col>
       </Row>
     </div>
--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,6 +1,33 @@
-import React from "react";
-const ProductInfo = () => {
-  return <div>ProductInfo</div>;
+import React, { useState, useEffect } from "react";
+import { Button, Descriptions } from "antd";
+const ProductInfo = (props) => {
+  const ClickHandler = (e) => {
+    e.preventDefault();
+    console.log("장바구니에 추가 하였씁니다.");
+  };
+  return (
+    <div>
+      <Descriptions title="ProductInfo">
+        <Descriptions.Item label="Price">
+          {props.detail.price}
+        </Descriptions.Item>
+        <Descriptions.Item label="Sold"> {props.detail.sold}</Descriptions.Item>
+        <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          {props.detail.description}
+        </Descriptions.Item>
+      </Descriptions>
+      <br />
+      <br />
+      <br />
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button size="large" shape="round" type="danger" onClick={ClickHandler}>
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductInfo;
